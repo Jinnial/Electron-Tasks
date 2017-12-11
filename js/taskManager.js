65429461e040e16217435f0f9feff21b89a50146
Brenden Reeves <reevesb@otc.edu>
2017-12-10 18:44:23 -0600

    Added Vue

--- js/taskManager.js
+++ js/taskManager.js
@@ -6,13 +6,26 @@ var fs =  require('fs');
 const ipc = electron.ipcRenderer;
 
 //Load Modals
-
 $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
 
-//Methods
-function addItem(){
-    alert('Adding item....');
-}
+//Vue.js
+var app = new Vue({
+    el: "#app",
+    data: {
+        //Name, Completed
+        items: [
+            {name: "Complete Final Project", completed: false}
+        ]
+    },
+    methods: {
+        addItem(task){
+            this.items.push({name: task, completed: false})
+        },
+        removeItem(){
+            alert('Removing Item....')
+        }
+    }
+});
